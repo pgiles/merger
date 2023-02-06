56196d8e79f627cee59ca1b257bc09c3c2213cd9
Paul Giles <pgiles@rccl.com>
2023-02-06 12:06:10 -0800

    clean up merger.go

--- merger.go
+++ merger.go
@@ -4,62 +4,92 @@ import (
 	"encoding/csv"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
-const outputFile = "output.csv"
+const outputFile = "merged.csv"
 
 func main() {
-
-	// clean up old output
-	if _, err := os.Stat(outputFile); !errors.Is(err, os.ErrNotExist) {
-		// path does not exist
-		os.Remove(outputFile)
-	}
-
-	// create a file writer
-	w, e := os.Create(outputFile)
-	if e != nil {
-		log.Panic("\nUnable to create outFile: ", e)
-	}
-	defer func(w *os.File) {
-		e = w.Close()
-		if e != nil {
-			log.Panic("\nUnable to close outFile: ", e)
-		}
-	}(w)
+	w := DeleteAndCreateFile(outputFile)
+	defer closeFile(w)
 
 	cw := csv.NewWriter(w)
-	csvMerge(cw, os.Args)
+	AppendCSVFiles(cw, os.Args[1:])
 }
 
-func csvMerge(w *csv.Writer, files []string) {
+// AppendCSVFiles appends the files in the array to the outputFile (writer)
+func AppendCSVFiles(w *csv.Writer, files []string) {
 	fmt.Println("input files:", files)
-	src := openFile(files[1])
-	defer src.Close()
-	csrc := csv.NewReader(src)
-	copy(csrc, w)
-	w.Flush()
 
-	for i := 1; i < len(files)-1; i++ {
-		fmt.Print(files[1] + " <- " + files[i+1] + "\n")
-		src := openFile(files[i+1])
+	for i := 0; i < len(files); i++ {
+		fmt.Printf("%v <- %s\n", outputFile, files[i])
+		src := openFile(files[i])
 		csvSrc := csv.NewReader(src)
 
-		copy(csvSrc, w)
-		src.Close()
+		copyTo(csvSrc, w)
+		closeFile(src)
 		w.Flush()
 	}
 }
 
+// DeleteAndCreateFile returned file is a resource that must be closed after the program is finished with it
+func DeleteAndCreateFile(f string) *os.File {
+	// delete old file
+	if _, err := os.Stat(f); !errors.Is(err, os.ErrNotExist) {
+		// path does not exist
+		err := os.Remove(f)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+
+	// create a file writer
+	w, e := os.Create(f)
+	if e != nil {
+		log.Panic("\nUnable to create output File: ", e)
+	}
+
+	return w
+}
+
+func closeFile(src *os.File) {
+	err := src.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
 func openFile(file string) *os.File {
-	// open the first file
+	// open the file
 	f, e := os.Open(file)
 	if e != nil {
-		log.Panic("\nUnable to open first file: ", e)
+		log.Panic("\nUnable to open file: ", e)
 	}
-	//defer f.Close()
 
 	return f
+}
+func readline(r *csv.Reader) ([]string, bool) {
+	line, e := r.Read()
+	if e != nil {
+		if e == io.EOF {
+			return nil, false
+		}
+		log.Panic("\nError reading file: ", e)
+	}
+	return line, true
+}
+
+func writeLine(w *csv.Writer, line []string) {
+	e := w.Write(line)
+	if e != nil {
+		log.Panic("\nError writing file: ", e)
+	}
+}
+
+func copyTo(r *csv.Reader, w *csv.Writer) {
+	for line, b := readline(r); b; line, b = readline(r) {
+		writeLine(w, line)
+	}
 }
