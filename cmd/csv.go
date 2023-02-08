2f2efdc6005cd09ab1537d4e013d4d3b73448bb9
Paul Giles <pgiles@rccl.com>
2023-02-08 10:30:19 -0800

    scaffold in CLI

--- cmd/csv.go
+++ cmd/csv.go
@@ -19,10 +19,39 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-package main
+package cmd
 
-import "github.com/pgiles/merger/cmd"
+import (
+	"fmt"
 
-func main() {
-	cmd.Execute()
+	"github.com/spf13/cobra"
+)
+
+// csvCmd represents the csv command
+var csvCmd = &cobra.Command{
+	Use:   "csv",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("csv called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(csvCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// csvCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// csvCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
